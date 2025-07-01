4c024fea93d65a546a9c653508925d38503c99f0
James Wu <jameswu21@g.ucla.edu>
2025-06-30 23:52:35 -0700

    Updated funfacts

--- src/components/bin/FunFact.tsx
+++ src/components/bin/FunFact.tsx
@@ -1,19 +1,13 @@
 const facts = [
-    "currently, my favorite color scheme is kanagawa and my favorite font is jetbrains mono.",
-    "while i enjoy using jetbrains IDEs, neovim is my current favorite code editor.",
-    "my favorite video game is oldschool runescape. runner-up is minecraft.",
-    "the last book i finished was carmilla. i'm currently reading atomic habits.",
-    "i prefer 2 spaces for tabs over 4 spaces.",
-    "my favorite classes so far at ucla have been introductory sociology (SOCIOL 1), algorithms and complexity (COM SCI 180), data management systems (COM SCI 143), and computer network fundamentals (COM SCI 118).",
-    "LaTeX is the superior note-taking system.",
-    "i enjoy mechanical keyboards, but i'm not very skilled in building them.",
+	'Some of my hobbies include working out, playing pickleball, and playing video games!',
+	'One of the things I enjoy is learning how successful businesses came to be.',
+	"I'm mildly red-green colorblind. Upon hearing this, most people point to the nearest red object and ask me what color it is.",
+	'I enjoy public speaking!',
+	'I have perfect pitch! (Which is great for violin!)',
+	'At UCLA, I enjoyed taking late night walks around campus.',
 ];
 
 export const FunFact = () => {
-    const fact = facts[Math.floor(Math.random() * facts.length)];
-    return (
-        <div>
-            {fact}
-        </div>
-    );
+	const fact = facts[Math.floor(Math.random() * facts.length)];
+	return <div>{fact}</div>;
 };
